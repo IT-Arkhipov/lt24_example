42f3ea804bd8df93bc444de03c1ee0f6b9063a17
Andrei Arkhipov <it.arkhipov@gmail.com>
2023-12-19 18:34:19 +0300

    initial with poetry

---
+++ test_mock.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+class TestMockResults:
+
+    def test_pass(self):
+        assert True
+
+    def test_fail(self):
+        assert False
+
+    @pytest.mark.skip
+    def test_skip(self):
+        assert True
+
+    @pytest.mark.xfail
+    def test_xfail_fail(self):
+        pytest.fail()
+
+    @pytest.mark.xfail
+    def test_xfail_pass(self):
+        assert True
